83a6d663eb32ac29fd5dfcc366ca946971790083
flis_ann <ann.flismark@gmail.com>
2020-10-22 11:06:35 +0200

    adding test one and two

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,5 +1,6 @@
-describe('usesr can create a contact', () => {
+describe('user can create a contact', () => {
     it('test if user can reach the site', () => {
-        cy.visit('http://localhost:3001')
+        cy.visit('http://localhost:3001') //go to and find localhost
+        cy.get('#add-contact').click() //find button called add-contact
     })
 })
