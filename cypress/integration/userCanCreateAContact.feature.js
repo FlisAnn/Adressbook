aba5df4ca2f490b31f1d0bd07afcb747c0f718f2
flis_ann <ann.flismark@gmail.com>
2020-10-22 14:06:00 +0200

    Added new test for the form and added html code to create a form

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,5 +2,11 @@ describe('user can create a contact', () => {
     it('test if user can reach the site', () => {
         cy.visit('http://localhost:3001') //go to and find localhost
         cy.get('#add-contact').click() //find button called add-contact
+        cy.get('#name').type('Pip')
+        cy.get('#email').type('pip@pipan.se')
+        cy.get('#phone').type('070 123123')
+        cy.get('#company').type('Pip & Mio')
+        cy.get('#notes').type('Awesome cat')
+        cy.get('#twitter').type('@pipan')
     })
 })
