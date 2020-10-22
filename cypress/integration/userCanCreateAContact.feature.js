665b389570464131a65a97e916f5d675e44a4eee
flis_ann <ann.flismark@gmail.com>
2020-10-22 15:18:05 +0200

    Adding test and functionality to toogle Add contact button. Adding css folder and file

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,5 +1,5 @@
 describe('user can create a contact', () => {
-    it('test if user can reach the site', () => {
+    beforeEach('test if user can reach the site', () => {
         cy.visit('http://localhost:3001') //go to and find localhost
         cy.get('#add-contact').click() //find button called add-contact
         cy.get('#name').type('Pip')
